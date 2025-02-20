f4bffd53008c9ae4aa678e9c28dd92d05657eedf
hong <honglin@andrew.cmu.edu>
2025-02-20 02:29:17 -0500

    fix authentication errors

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,18 +1,43 @@
 import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
 import { CreateWSSContextFnOptions } from '@trpc/server/src/adapters/ws';
-import { getSession } from 'next-auth/react';
+import { auth } from '../auth/auth';
+import { GetServerSidePropsContext } from 'next';
+import { ServerResponse } from 'http';
+
+function isCreateNextContextOptions(
+  opts: CreateNextContextOptions | CreateWSSContextFnOptions,
+): opts is CreateNextContextOptions {
+  return 'getHeaders' in opts.res;
+}
 
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/v11/context
  */
 export const createContext = async (
-  opts: CreateNextContextOptions |  CreateWSSContextFnOptions
+  opts: CreateNextContextOptions | CreateWSSContextFnOptions,
 ) => {
-  const session = await getSession(opts);
-  return {
-    session
+  if (isCreateNextContextOptions(opts)) {
+    const session = await auth(opts.req, opts.res);
+    return { session };
   }
+
+  const req: GetServerSidePropsContext['req'] = Object.assign(
+    { cookies: {} },
+    opts.req,
+  );
+
+  const res = {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    getHeader() {},
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    setCookie() {},
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    setHeader() {},
+  } as unknown as ServerResponse;
+
+  const session = await auth(req, res);
+  return { session };
 };
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
