630e78bd51362364f289af088ab4c91bd6c6d5d6
hong <honglin@andrew.cmu.edu>
2025-03-11 19:05:12 -0400

    add some checks to build process

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+const config: NextConfig = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    APP_URL: process.env.APP_URL,
+    WS_URL: process.env.WS_URL,
+  },
+  /** We run eslint as a separate task in CI */
+  eslint: {
+    ignoreDuringBuilds:
+      !!process.env.CI || process.env.BUILD_ENV === 'production',
+  },
+  typescript: {
+    ignoreBuildErrors:
+      !!process.env.CI || process.env.BUILD_ENV === 'production',
+  },
+};
+
+export default withNextIntl(config);
